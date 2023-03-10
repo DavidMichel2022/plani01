2a45725185087ecd3cb698220381fea836093136
David Michel Lopez <David Michel Lopez@MICHELSISTEMAS>
2023-03-10 13:33:06 -0400

    Modificacion Tabla UnidadProceso

--- SistemaPlanificacion.AplicacionWeb/Models/ViewModels/VMPartidaPresupuestaria.cs
+++ SistemaPlanificacion.AplicacionWeb/Models/ViewModels/VMPartidaPresupuestaria.cs
@@ -5,18 +5,10 @@ namespace SistemaPlanificacion.AplicacionWeb.Models.ViewModels
     public class VMPartidaPresupuestaria
     {
         public int IdPartida { get; set; }
-
         public string? Codigo { get; set; }
         public string? Nombre { get; set; }
-
         public int? Stock { get; set; }
-
+        public int? IdPrograma { get; set; }
         public int? EsActivo { get; set; }
-
-        public DateTime? FechaRegistro { get; set; }
-
-        public virtual ICollection<DetallePlanificacion> DetallePlanificacions { get; } = new List<DetallePlanificacion>();
-
-        public virtual ICollection<MoviPlanificacion> MoviPlanificacions { get; } = new List<MoviPlanificacion>();
     }
 }
