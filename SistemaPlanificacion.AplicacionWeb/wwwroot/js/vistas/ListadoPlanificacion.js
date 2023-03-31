6c14870675b92aae00a1adb2e410cb098272e702
Dtic Sistemas CNS <dtic.sistemas.cns@gmail.com>
2023-03-31 10:41:05 -0400

    Adicionar Opcion Anular y visualizacion de Estados en listado de Planificacion

--- SistemaPlanificacion.AplicacionWeb/wwwroot/js/vistas/ListadoPlanificacion.js
+++ SistemaPlanificacion.AplicacionWeb/wwwroot/js/vistas/ListadoPlanificacion.js
@@ -23,27 +23,46 @@ $(document).ready(function () {
             { "data": "nombreRegional" },
             { "data": "nombreEjecutora" },
             { "data": "montoPlanificacion" },
-            { "data": "idCentro" 
-                /*"data": "estadoCarpeta", render: function (data) {
-                    if (data == 1)
-                        return '<span class="badge badge-info">Certificado</span>';
-                    else
-                        return '<span class="badge badge-warning">Pendiente</span>';
-                }*/
+            {
+                "data": "estadoCarpeta", render: function (data) {
+                    if (data == "INI") {
+                        return `<span class="badge badge-info">INICIAL</span>`;
+                    }
+                    else {
+                        if (data == "ANU") {
+                            return '<span class="badge badge-secondary">ANULADO</span>';
+                        }
+                        else {
+                            if (data == "PLA") {
+                                return '<span class="badge badge-success">PLANIFICADO</span>';
+                            }
+                            else {
+                                return '<span class="badge badge-danger">OTRO</span>';
+                            }
+                        }
+                    }
+
+                }
             },
-            //{ "data": "unidadSolicitante" },
             { "data": "fechaPlanificacion" },
             {
-                "defaultContent": '<div class="form-inline">' +
-                    '<button class="btn btn-info btn-certificar btn-sm" href=""><i class="fas fa-edit"></i> Certificar</button> ' +
-                    '<button class="btn btn-primary btn-ver btn-sm"><i class="fas fa-eye"></i> Ver</button> ' +
-                    '<button class="btn btn-default btn-imprimir btn-sm"><i class="fas fa-print"></i></button>' +
-                    '<button class="btn btn-danger btn-eliminar btn-sm"><i class="fas fa-trash-alt"></i></button>' +
-                    '</div>',
-                "orderable": false,
+                "data": "estadoCarpeta", render: function (data) {
+                    var swBtnEdit = '<button class="btn btn-info btn-certificar btn-sm" href=""><i class="fas fa-edit"></i> Certificar</button> ';
+                   if (data == "PLA") {
+                       swBtnEdit = '<button class="btn btn-info btn-certificar btn-sm" href=""><i class="fas fa-edit"></i> Modificar</button> ';                                               
+                    }
+                    return '<div class="form-inline">' + swBtnEdit +
+                        '<button class="btn btn-primary btn-ver btn-sm"><i class="fas fa-eye"></i> Ver</button> ' +
+                        '<button class="btn btn-default btn-imprimir btn-sm"><i class="fas fa-print"></i></button>' +
+                        '<button class="btn btn-danger btn-eliminar btn-sm"><i class="fas fa-trash-alt"></i></button>' +
+                        '</div>'                        
+
+                }, "orderable": false,
                 "searchable": false,
                 "width": "80px"
-            }
+
+                
+            }           
         ],
         order: [[0, "desc"]],
         dom: "Bfrtip",
@@ -82,28 +101,91 @@ $("#tbdata tbody").on("click", ".btn-certificar", function () {
 
 })
 
-
-
 $("#tbdata tbody").on("click", ".btn-eliminar", function () {
 
-    let fila;
+    const modelo = structuredClone(MODELO_BASE);
 
     if ($(this).closest("tr").hasClass("child")) {
-        fila = $(this).closest("tr").prev();
+        filaSeleccionada = $(this).closest("tr").prev();
     }
     else {
-        fila = $(this).closest("tr")
+        filaSeleccionada = $(this).closest("tr")
     }
 
-    const data = tablaData.row(fila).data();
+    const data = tablaData.row(filaSeleccionada).data();
+
+    $("#txtId").val(data.idPlanificacion)
+    $("#txtIdDocumento").val(data.idDocumento)
+    $("#txtIdCentro").val(data.idCentro)
+    $("#txtIdUnidadResponsable").val(data.idUnidadResponsable)
+    $("#txtIdUsuario").val(data.idUsuario)
+    $("#txtLugar").val(data.lugar)
+    $("#txtCertificadoPoa").val(data.certificadoPoa)
+    $("#txtReferenciaPlanificacion").val(data.referenciaPlanificacion)
+    $("#txtNombreRegional").val(data.nombreRegional)
+    $("#txtNombreEjecutora").val(data.nombreEjecutora)
+    $("#txtMontoPoa").val(data.montoPoa)
+    $("#txtMontoPresupuesto").val(data.montoPresupuesto)
+    $("#txtMontoCompra").val(data.montoCompra)
+    $("#txtUnidadProceso").val(data.unidadProceso)
+    $("#txtFechaRegistro").val(data.fechaPlanificacion)
+    $("#txtNumeroPlanificacion").val(data.numeroPlanificacion)
+    $("#txtCitePlanificacion").val(data.citePlanificacion)
+    $("#txtUnidadSolicitante").val(data.nombreCentro)
+    $("#txtUnidadResponsable").val(data.nombreUnidadResponsable)
+    $("#txtEstadoCarpeta").val(data.estadoCarpeta)
+    $("#txtObservacion").val(data.estadoCarpeta)
+    $("#txtTotal").val(data.montoPlanificacion)
+
+    if (data.estadoCarpeta == "INI") {
+        $("#txtObservacion").val("")
+    }
+    else {
+        if (data.estadoCarpeta == "ANU") {
+            $("#txtObservacion").val("ANULADO")
+        }
+        else {
+            $("#txtObservacion").val("")
+        }
+    }
+
+    $("#tbPartida tbody").html("")
+    cont = 0;
+    data.detallePlanificacion.forEach((item) => {
+        cont++;
+        $("#tbPartida tbody").append(
+            $("<tr>").append(
+                $("<td>").text(cont),
+                $("<td>").text(item.nombreItem),
+                $("<td>").text(item.medida),
+                $("<td>").text(item.cantidad),
+                $("<td>").text(item.precio),
+                $("<td>").text(item.total),
+                $("<td>").text(item.codigoActividad),
+            )
+        )
+    })
+    $("#linkImprimir").attr("href", `/Planificacion/MostrarPDFCarpeta?numeroCarpeta=${data.numeroCarpeta}`);
+
+
+    if ($("#txtEstadoCarpeta").val() == "ANU") {
+        swal("Atencion", "Carpeta De Planificacion Ya Esta Anulada!", "warning");
+        return;
+    }
 
     swal({
-        title: "Está Seguro?",
-        text: `Eliminar La Carpeta de Requerimiento: "${data.numeroCarpeta}"`,
+        title: "Está Seguro de Anular?",
+        text: '\n' + `Carpeta Planificacion: N°. "${data.numeroPlanificacion}"` + "\n" +
+            `N°. Cite: "${data.citePlanificacion}` + "\n" + "\n" +
+            `Fecha Registro: "${data.fechaPlanificacion}"` + "\n" +
+            `Importe Total: "${data.montoPlanificacion}"` + "\n" + "\n" +
+            `Unidad Solicitante: "${data.nombreCentro}"` + "\n" +
+            `Unidad Responsable: "${data.nombreUnidadResponsable}"` + "\n",
+
         type: "warning",
         showCancelButton: true,
         confirmButtonClass: "btn-danger",
-        confirmButtonText: "Si, Eliminar",
+        confirmButtonText: "Si, Anular",
         cancelButtonText: "No, Cancelar",
         closeOnConfirm: false,
         closeOnCancel: true,
@@ -111,30 +193,45 @@ $("#tbdata tbody").on("click", ".btn-eliminar", function () {
         function (respuesta) {
             if (respuesta) {
                 $(".showSweetalert").LoadingOverlay("show");
-                alert('TEST');
-                /* fetch(`/Empresa/Eliminar?IdEmpresa=${data.idEmpresa}`, {
-                     method: "DELETE"
-                 })
-                     .then(response => {
-                         $(".showSweetalert").LoadingOverlay("hide");
-                         return response.ok ? response.json() : Promise.reject(response);
-                     })
-                     .then(responseJson => {
- 
-                         if (responseJson.estado) {
- 
-                             tablaData.row(fila).remove().draw()
- 
-                             swal("Listo!", "La Empresa Fue Eliminada", "success")
-                         }
-                         else {
-                             swal("Lo Sentimos", responseJson.mensaje, "error")
-                         }
-                     })*/
+
+                const modelo = structuredClone(MODELO_BASE);
+
+                modelo["idPlanificacion"] = parseInt($("#txtId").val())
+                modelo["estadoCarpeta"] = "ANU"
+
+                //console.log(modelo);
+                //alert(modelo["idPlanificacion"]);
+
+                fetch("/Planificacion/Anular", {
+                    method: "PUT",
+                    headers: { "Content-Type": "application/json; charset=utf-8" },
+                    body: JSON.stringify(modelo)
+                })
+                    .then(response => {
+                        $("#modalData").find("div.modal-content").LoadingOverlay("hide");
+                        return response.ok ? response.json() : Promise.reject(response);
+                    })
+                    .then(responseJson => {
+
+                        if (responseJson.estado) {
+
+                            tablaData.row(filaSeleccionada).data(responseJson.objeto).draw(false);
+
+                            filaSeleccionada = null;
+
+                            swal("Listo!", "La Carpeta De Planificacion Fue Anulada", "success")
+                            let nueva_url = `/Planificacion/ListaMisCarpetas`;
+                            tablaData.ajax.url(nueva_url).load();
+                        }
+                        else {
+                            swal("Lo Sentimos", responseJson.mensaje, "error")
+                        }
+                    })
             }
         }
     )
 })
+
 
 $("#tbdata tbody").on("click", ".btn-ver", function () {
     if ($(this).closest("tr").hasClass("child")) {
