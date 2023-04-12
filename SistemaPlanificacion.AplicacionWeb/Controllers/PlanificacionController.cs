1005882f7f36929f3c57445c1e2fdfcdc08ef815
David Michel Lopez <David Michel Lopez@MICHELSISTEMAS>
2023-04-12 11:17:21 -0400

    Modificacion de Adan

--- SistemaPlanificacion.AplicacionWeb/Controllers/PlanificacionController.cs
+++ SistemaPlanificacion.AplicacionWeb/Controllers/PlanificacionController.cs
@@ -156,7 +156,7 @@ namespace SistemaPlanificacion.AplicacionWeb.Controllers
             return StatusCode(StatusCodes.Status200OK, gResponse);
         }
 
-        [HttpDelete]
+        [HttpGet]
         public async Task<IActionResult> Eliminar(int idPlanificacion)
         {
             GenericResponse<string> gResponse = new GenericResponse<string>();
