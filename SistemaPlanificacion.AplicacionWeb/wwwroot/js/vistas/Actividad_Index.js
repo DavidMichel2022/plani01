5ac1484fda50942e24a5fab4bca6d522413680a7
David Michel Lopez <David Michel Lopez@MICHELSISTEMAS>
2023-04-21 15:09:25 -0400

    Revision Ventana Modal

--- SistemaPlanificacion.AplicacionWeb/wwwroot/js/vistas/Actividad_Index.js
+++ SistemaPlanificacion.AplicacionWeb/wwwroot/js/vistas/Actividad_Index.js
@@ -1,72 +1,4 @@
-﻿const formulario = document.getElementById('formulario');
-const inputs = document.querySelectorAll('#formulario input');
-
-const expresiones = {
-    codigo: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-    nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-}
-
-const campos = {
-    codigo: false,
-    nombre: false,
-}
-
-const validarFormulario = (e) => {
-    switch (e.target.name) {
-        case "codigo":
-            validarCampo(expresiones.codigo, e.target, 'codigo');
-            break;
-        case "nombre":
-            validarCampo(expresiones.nombre, e.target, 'nombre');
-            break;
-    }
-}
-
-const validarCampo = (expresion, input, campo) => {
-    if (expresion.test(input.value)) {
-        document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
-        document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
-        document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
-        document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
-        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
-        campos[campo] = true;
-    } else {
-        document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
-        document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
-        document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
-        document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
-        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
-        campos[campo] = false;
-    }
-}
-
-inputs.forEach((input) => {
-    input.addEventListener('keyup', validarFormulario);
-    input.addEventListener('blur', validarFormulario);
-});
-
-formulario.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const terminos = document.getElementById('terminos');
-    if (campos.codigo && campos.nombre) {
-        formulario.reset();
-
-        document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
-        setTimeout(() => {
-            document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
-        }, 2000);
-
-        document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
-            icono.classList.remove('formulario__grupo-correcto');
-        });
-
-    } else {
-        document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
-    }
-});
-
-const MODELO_BASE = {
+﻿const MODELO_BASE = {
     idActividad: 0,
     codigo:"",
     nombre: "",
@@ -137,25 +69,24 @@ $("#btnNuevo").click(function () {
     mostrarModal()
 })
 
+$("#cancelarmodalDataNuevo").click(function () {
+    $("#modalDataNuevo").modal("hide")
+})
 
-
-//$("#btnCancelar").click(function () {
-//    formulario.reset();
-//    //$("#modalDataNuevo").modal("hide")
-//})
-
-$("#btnGuardar").click(function () {
+$("#guardarmodalDataNuevo").click(function () {
 
     //debugger;
 
-    if ($("#txtCodigo").val().trim() == "") {
+    alert($("#codigo").val());
+
+    if ($("#codigo").val().trim() == "") {
         toastr.warning("", "No Deje En Blanco El Codigo De Actividad")
-        $("#txtCodigo").focus()
+        $("#codigo").focus()
         return;
     }
-    if ($("#txtNombre").val().trim() == "") {
+    if ($("#nombre").val().trim() == "") {
         toastr.warning("", "No Deje En Blanco Nombre De Actividad")
-        $("#txtNombre").focus()
+        $("#nombre").focus()
         return;
     }
 
@@ -175,7 +106,7 @@ $("#btnGuardar").click(function () {
             body: JSON.stringify(modelo)
         })
             .then(response => {
-                $("#modalData").find("div.modal-content").LoadingOverlay("hide");
+                $("#modalDataNuevo").find("div.modal-content").LoadingOverlay("hide");
                 return response.ok ? response.json() : Promise.reject(response);
             })
             .then(responseJson => {
@@ -183,7 +114,7 @@ $("#btnGuardar").click(function () {
                 if (responseJson.estado) {
 
                     tablaData.row.add(responseJson.objeto).draw(false)
-                    $("#modalData").modal("hide")
+                    $("#modalDataNuevo").modal("hide")
                     swal("Listo!", "La Actividad Fue Creada", "success")
                 }
                 else {
@@ -198,7 +129,7 @@ $("#btnGuardar").click(function () {
             body: JSON.stringify(modelo)
         })
             .then(response => {
-                $("#modalData").find("div.modal-content").LoadingOverlay("hide");
+                $("#modalDataNuevo").find("div.modal-content").LoadingOverlay("hide");
                 return response.ok ? response.json() : Promise.reject(response);
             })
             .then(responseJson => {
@@ -209,7 +140,7 @@ $("#btnGuardar").click(function () {
 
                     filaSeleccionada = null;
 
-                    $("#modalData").modal("hide")
+                    $("#modalDataNuevo").modal("hide")
                     swal("Listo!", "La Actividad Fue Modificada", "success")
                 }
                 else {
