2982e24a72f20f6251cc6750dd99bf47368e221f
Dtic Sistemas CNS <dtic.sistemas.cns@gmail.com>
2023-04-25 09:54:46 -0400

    Lectura de Archivos Excel Preformato POA

---
+++ SistemaPlanificacion.AplicacionWeb/Models/ViewModels/VMRequerimientoPoa.cs
@@ -0,0 +1,14 @@
+﻿namespace SistemaPlanificacion.AplicacionWeb.Models.ViewModels
+{
+    public class VMRequerimientoPoa
+    {
+        public string numero { get; set; }
+        public string partida { get; set; }
+        public string detalle { get; set; }
+        public string unidad { get; set; }
+        public string cantidad { get; set; }
+        public string precioUnitario { get; set; }
+        public string precioTotal { get; set; }
+
+    }
+}
---
+++ SistemaPlanificacion.AplicacionWeb/wwwroot/js/vistas/RequerimientoPoa_Index.js
@@ -0,0 +1,34 @@
+﻿function mostrarDatos() {
+
+    const input = document.getElementById("inputExcel");
+    const formData = new FormData();
+    formData.append("ArchivoExcel", input.files[0]);
+
+    fetch("/RequerimientoPoa/MostrarDatos", {
+        method: "POST",
+        body: formData
+    })
+        .then(response => {
+            console.log(response);
+            return response.json()
+        })
+        .then(responseJson => {
+            console.log(responseJson);
+            responseJson.forEach((item) => {
+                $("#tbData tbody").append(
+                    $("<tr>").append(                        
+                        $("<td>").text(item.numero),
+                        $("<td>").text(item.partida),
+                        $("<td>").text(item.detalle),
+                        $("<td>").text(item.unidad),
+                        $("<td>").text(item.cantidad),
+                        $("<td>").text(item.precioUnitario),
+                        $("<td>").text(item.precioTotal)
+                    )
+                )
+            })
+
+        })
+
+  
+}
